091f01cf75ec8b65a59c83dc4cc27d2231ea8f2c
Sheikh-Tafsir <190041130tafsir@gmail.com>
2023-09-24 13:19:10 +0600

    some lines fixed

--- src/pages/landingPage/Sponsors.jsx
+++ src/pages/landingPage/Sponsors.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 import '../../styles/Sponsors.css'
 const Sponsors = () => {
   return (
-    <>
-        <div className='sponsors'>
+    <div className='sponsors'>
+        <div className='sponsorsSubBar'>
+            <h2 className='sponsorsTitle'>Our sponsors</h2>     
             <div className='w-3/4 h-1/3 mx-auto my-auto'>
                 <div className="h-full overflow-x-auto whitespace-nowrap slider">
                     <div className="inline-block imgBar">
@@ -21,19 +22,10 @@ const Sponsors = () => {
                     <div className="inline-block imgBar">
                         <img src='/cephalo.jpeg' alt='' />
                     </div>
-                    <div className="inline-block imgBar">
-                        <img src='https://premiumlayers.com/wpthemes/museum/wp-content/uploads/2015/10/event-img-4-289x365.jpg' alt='' />
-                    </div>
-                    <div className="inline-block imgBar">
-                        <img src='https://premiumlayers.com/wpthemes/museum/wp-content/uploads/2015/10/event-img-4-289x365.jpg' alt='' />
-                    </div>
-                    <div className="inline-block imgBar">
-                        <img src='https://premiumlayers.com/wpthemes/museum/wp-content/uploads/2015/10/event-img-4-289x365.jpg' alt='' />
-                    </div>
                 </div>
             </div>
         </div>
-    </>
+    </div>
   )
 }
 
